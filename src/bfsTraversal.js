7ebddd132a02ebfae859d2002af0846732ef14e1
ayoaditya <adityakhatana9@gmail.com>
2024-07-11 18:42:01 +0530

    Change in Bfs code

--- src/bfsTraversal.js
+++ src/bfsTraversal.js
@@ -59,7 +59,7 @@ export function bfsTraversal(connections, isWeighted) {
     const visited = new Set();
     const bfsOrder = [];
 
-    // Assume DistinctNodes is a Set containing all distinct nodes
+    // set of DistinctNodes in Graph
     const DistinctNodes = new Set(Object.keys(graph)); // Initialize with all keys in the graph
 
     const bfs = (startNode) => {
@@ -149,62 +149,3 @@ export function bfsTraversal(connections, isWeighted) {
     return { nodes: newNodes, edges: newEdges };
   }
 }
-
-// {
-//     canvas: { background: '#fff' },
-//     node:
-//     {
-//       fill: '#7CA0AB',
-//       activeFill: '#1DE9AC',
-//       opacity: 1,
-//       selectedOpacity: 1,
-//       inactiveOpacity: 0.2,
-//       label: {
-//         color: '#2A6475',
-//         stroke: '#fff',
-//         activeColor: '#1DE9AC'
-//       },
-//       subLabel: {
-//         color: '#ddd',
-//         stroke: 'transparent',
-//         activeColor: '#1DE9AC'
-//       }
-//     },
-
-//     lasso: {
-//       border: '1px solid #55aaff',
-//       background: 'rgba(75, 160, 255, 0.1)'
-//     },
-//     ring: {
-//       fill: '#D8E6EA',
-//       activeFill: '#1DE9AC'
-//     },
-//     edge:
-//     {
-//       fill: '#D8E6EA',
-//       activeFill: '#1DE9AC',
-//       opacity: 1,
-//       selectedOpacity: 1,
-//       inactiveOpacity: 0.1,
-//       label: {
-//         stroke: '#fff',
-//         color: '#2A6475',
-//         activeColor: '#1DE9AC',
-//         fontSize: 6
-//       }
-//     },
-//     arrow: {
-//       fill: '#D8E6EA',
-//       activeFill: '#1DE9AC'
-//     },
-//     cluster: {
-//       stroke: '#D8E6EA',
-//       opacity: 1,
-//       selectedOpacity: 1,
-//       inactiveOpacity: 0.1,
-//       label: {
-//         stroke: '#fff',
-//         color: '#2A6475'
-//       }
-//     }
-//   }
