dfa6818529099245e7f38b2fd95a568b80d2cba1
ayoaditya <adityakhatana9@gmail.com>
2024-08-02 15:34:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -26,6 +26,11 @@ function App() {
   const [isUnweightedChecked, setIsUnweightedChecked] = useState(true);
 
   // Handlers to update the state
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
   const handleWeightedChange = (event) => {
     const checked = event.target.checked;
     setIsWeightedChecked(checked);
@@ -293,6 +298,8 @@ function App() {
             >
               Find Shortest Path
             </button>
+            <button onclick={reloadPage}>Reload</button>
+
             <h2 className="FSPDist">
               Shortest Distance Between {sourceValue} And {destinationValue} Is:{" "}
               {shortestDistance}
